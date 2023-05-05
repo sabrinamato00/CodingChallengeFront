a9a0b10ac74e5c19c4cd3a2b4d75e33bdfe4131f
sabirnamato00 <sabrinamato00@gmail.com>
2023-05-04 21:56:07 -0300

    PublicacionesRecientes

---
+++ pop_some_tags/src/Components/PublicacionesRecientes.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./PublicacionesRecientes.css";
+
+function PublicacionesRecientes() {
+  return (
+    <div className="publicacionesRecientes">
+      <div className="cardPublicacion">
+        <img src="./Img/Publi1.png" alt="" />
+        <h2>Less new</h2>
+        <p>
+          Reduce, reutiliza y recicla. Juntos podemos hacer la diferencia para
+          cambiar el planeta.
+        </p>
+        <button>Leer más</button>
+      </div>
+      <div className="cardPublicacion">
+        <img src="./Img/Publi2.png" alt="" />
+        <h2> Ferias circulares</h2>
+        <p>
+          Compra productos locales, sostenibles y éticos. Haz un cambio positivo
+          hoy mismo!
+        </p>
+        <button>Leer más</button>
+      </div>
+      <div className="cardPublicacion">
+        <img src="./Img/Publi3.png" alt="" />
+        <h2>Sustentabilidad</h2>
+        <p>
+          Empoderate para cuidar el planeta. Pequenas acciones diarias pueden
+          tener un gran impacto.
+        </p>
+        <button>Leer más</button>
+      </div>
+    </div>
+  );
+}
+
+export default PublicacionesRecientes;
